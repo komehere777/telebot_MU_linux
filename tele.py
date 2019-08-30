93e7a2382b6b07f0f6c31cae9b7bc85b5dcaf982
komeh <komehere@knou.ac.kr>
2019-08-30 20:59:55 +0900

    first

--- tele.py
+++ tele.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 req = requests.get('https://m.onoffmix.com/event/main?s=%EB%B0%8B%EC%97%85')
 html = req.text
-soup = BeautifulSoup(html, 'html5lib')
+soup = BeautifulSoup(html, 'html.parser')
 all_titles = soup.find_all('h1', {"class": "event_title"})
 for title in all_titles:
     print(title.get_text())
